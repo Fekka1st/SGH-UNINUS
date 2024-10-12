8d3893c0168197df989cd3ac5af583e73d5a2b24
Aby Herdiansyah <144993262+Abyhrdians@users.noreply.github.com>
2024-10-12 16:17:08 +0700

    baru

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,3 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-// Authentication routes
-require __DIR__.'/auth.php';
