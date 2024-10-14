5e2e0e1c3de279b4447ab3a5cf6cd1a70045df27
Ferry Aditya H <ferryaditya124@gmail.com>
2024-10-15 01:43:55 +0700

    #add data

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -430,40 +430,8 @@
             <!-- Start Footer Top -->
             <div class="footer-top">
                 <div class="container">
-                    <div class="row">
-                        <div class="col-lg-4 col-md-4 col-12">
-                            <div class="single-footer f-about">
-                                <div class="logo">
-                                    <a href="index.html">
-                                        <img src="/earsip/assets/images/logo/white-logo.webp" alt="#">
-                                    </a>
-                                </div>
-                                <p>Smart Green House adalah sebuah aplikasi yang membantu meningkatkan kualitas hidup
-                                    dengan
-                                    mengintegrasikan teknologi dan alam. Kami berfokus pada pengembangan sistem yang
-                                    ramah
-                                    lingkungan dan berkelanjutan.
-                                    <ul class="social">
-                                        <li><a href="https://www.instagram.com/uninusbandung/" target="_blank"><i
-                                                    class="lni lni-instagram"></i></a></li>
-                                        <li><a href="https://www.youtube.com/@uninusbandung" target="_blank"><i
-                                                    class="lni lni-youtube"></i></a></li>
-                                    </ul>
-                            </div>
-                        </div>
-                        <div class="col-lg-8 col-md-8 col-12">
-                            <div class="row">
-                                <div class="col-lg-3 col-md-6 col-12">
-                                    <div class="single-footer f-link">
-                                        <h3>Kontak</h3>
-                                        <p>Jl. Soekarno Hatta No.530, Sekejati, Kec. Buahbatu, Kota Bandung, Jawa Barat
-                                        </p>
-                                        <p><i class="lni lni-phone"></i> (022) 21000229 </br>
-                                            <i class="lni lni-envelope"></i> https://uninus.ac.id/</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                    <div class="row align-items-start">
+                        
                     </div>
                 </div>
             </div>
