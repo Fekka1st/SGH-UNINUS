1e4966fbb600f96e0e769c7a16baa0f30e84654c
data <data@data.com>
2024-10-29 17:39:16 +0700

    memperbaharui tentang kami

--- resources/views/homepage/tentangkami.blade.php
+++ resources/views/homepage/tentangkami.blade.php
@@ -51,7 +51,7 @@
                 <!-- Menambah jarak bawah untuk mobile -->
                 <div class="single-table" style="height:100%; background-color: #1C532A;font-family: 'Times New Roman', Times, serif; " data-wow-delay=".2s">
                     <div class="text-center">
-                        <h4 class="title" style=" margin-top: 50px; color: white;">Fungsi</h4>
+                        <h3 class="title" style=" margin-top: 50px; color: white;">Fungsi</h3>
                         <p style="text-align: justify; margin-right: 35px; margin-bottom: 35px; margin-left: 35px; color: white;">Smart Greenhouse berfungsi sebagai sistem pertanian terintegrasi yang memanfaatkan teknologi sensor dan otomatisasi untuk mengatur dan memantau berbagai parameter lingkungan, seperti suhu, kelembapan, pencahayaan, dan tingkat nutrisi. Sistem ini juga membantu petani mengurangi risiko kegagalan panen dan memastikan kualitas hasil pertanian yang optimal setiap saat.</p>
                     </div>
                 </div>
@@ -60,7 +60,7 @@
             <div class="col-lg-4 col-md-6 col-12 mb-4" data-aos="zoom-in-down">
                 <div class="single-table" style="height:100%; background-color: #1C532A;font-family: 'Times New Roman', Times, serif;" data-wow-delay=".4s">
                     <div class="text-center">
-                        <h4 class="title" style=" margin-top: 50px; color: white;">Tujuan</h4>
+                        <h3 class="title" style=" margin-top: 50px; color: white;">Tujuan</h3>
                         <p style="text-align: justify;margin-right: 35px; margin-bottom: 35px; margin-left: 35px;color: white;">Tujuan utama pengembangan Smart Greenhouse adalah meningkatkan produktivitas pertanian modern melalui pemanfaatan data analitik dan teknologi IoT. Dengan adanya sistem yang cerdas ini, petani dapat lebih fokus pada kualitas, kuantitas, dan efektivitas pengelolaan lahan, sambil meminimalisir dampak negatif terhadap lingkungan.
                         </p>
                     </div>
@@ -69,7 +69,7 @@
             <div class="col-lg-4 col-md-6 col-12 mb-4" data-aos="zoom-in-down">
                 <div class="single-table" style="height:100%; background-color: #1C532A;font-family: 'Times New Roman', Times, serif;" data-wow-delay=".6s">
                     <div class="text-center">
-                        <h4 class="title" style=" margin-top: 50px; color: white;">Manfaat</h4>
+                        <h3 class="title" style=" margin-top: 50px; color: white;">Manfaat</h3>
                         <p style="text-align: justify; margin-right: 35px; margin-bottom: 35px; margin-left: 35px; color: white;">Penerapan Smart Greenhouse memberikan manfaat berupa penghematan air dan energi hingga 50%, serta pengurangan penggunaan pestisida berkat teknologi otomatisasi yang cerdas. Hal ini tidak hanya meningkatkan efisiensi produksi, tetapi juga memastikan pertanian yang lebih ramah lingkungan dan berkelanjutan.</p>
                     </div>
                 </div>
