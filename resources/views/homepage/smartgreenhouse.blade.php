385f019311bfb5c3257c66b46772866dc9674e71
 <anif2805@gmail.com>
2024-11-07 10:59:34 +0700

    Panel Temp

--- resources/views/homepage/smartgreenhouse.blade.php
+++ resources/views/homepage/smartgreenhouse.blade.php
@@ -2,21 +2,10 @@
 
 
 @section('content')
-<section class="hero-area">
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="brand_color">
-                <div style="text-align: center;">
-                    <h2 style="color: white;">
-                        <b>Smart Greenhouse</b>
-                    </h2>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section id="overview" class="app-info section" style="padding-top:50px;padding-bottom:50px">
+<section id="overview" class="app-info section">
+<div style="height: 200px; padding: 50px 0;">
+<h1>Smart Room GreenHouse</h1>
+</div>
 <div class="container-fluid">
 <div class="row">
 
@@ -28,13 +17,8 @@
                 <div class="col mr-2">
                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                         CO2
+                        <div id='co2Chart'></div>
                     </div>
-                        <div>
-                            2000PPM
-                        </div>
-                </div>
-                <div class="col-auto">
-                <i class="fab fa-cloudversify"></i>
                 </div>
             </div>
         </div>
@@ -46,14 +30,9 @@
             <div class="row no-gutters align-items-center">
                 <div class="col mr-2">
                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                        Intensitas Cahaya
+                        Light Intensity
+                        <div id='cahayaChart'></div>
                     </div>
-                        <div>
-                            2000 lx
-                        </div>
-                </div>
-                <div class="col-auto">
-                    <i class="fas fa-lightbulb"></i>
                 </div>
             </div>
         </div>
@@ -65,52 +44,51 @@
             <div class="row no-gutters align-items-center">
                 <div class="col mr-2">
                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                        Pemakaian Air
+                        Water Volume
+                        <div id='airChart'></div>
                     </div>
-                        <div>
-                            1000 Liter
-                        </div>
-                </div>
-                <div class="col-auto">
-                    <i class="fas fa-water"></i>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<div class="col-xl-6 col-md-6 mb-4">
+<div class="col-xl-4 col-md-6 mb-4">
     <div class="card border-left-primary shadow h-100 py-2">
         <div class="card-body">
             <div class="row no-gutters align-items-center">
                 <div class="col mr-2">
                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                        Rata-Rata Suhu
+                        Temp Average
+                        <div id='myChart'></div>
                     </div>
-                        <div>
-                            30°C
-                        </div>
-                </div>
-                <div class="col-auto">
-                    <i class="fas fa-thermometer-full"></i>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<div class="col-xl-6 col-md-6 mb-4">
+<div class="col-xl-4 col-md-6 mb-4">
     <div class="card border-left-primary shadow h-100 py-2">
         <div class="card-body">
             <div class="row no-gutters align-items-center">
                 <div class="col mr-2">
                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                        Rata-Rata Kelembapan
+                        Room Panel Temp
+                        <div id='panelChart'></div>
                     </div>
-                        <div>
-                            50%
-                        </div>
                 </div>
-                <div class="col-auto">
-                <i class="fas fa-tint"></i>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="col-xl-4 col-md-6 mb-4">
+    <div class="card border-left-primary shadow h-100 py-2">
+        <div class="card-body">
+            <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                        Rh Average
+                        <div id='kelembabanChart'></div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -122,29 +100,29 @@
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2 text-center"> <!-- Tambahkan class text-center -->
                         <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                            Sensor Suhu
+                            Temperature Sensor
                         </div>
                         <div>
                             <div>
-                                <h5>Suhu 1 : 29°C</h5>
+                                <h5>Temp 1 : 29°C</h5>
                             </div>
                             <div>
-                                <h5>Suhu 2 : 28°C</h5>
+                                <h5>Temp 2 : 28°C</h5>
                             </div>
                             <div>
-                                <h5>Suhu 3 : 29°C</h5>
+                                <h5>Temp 3 : 29°C</h5>
                             </div>
                             <div>
-                                <h5>Suhu 4 : 28°C</h5>
+                                <h5>Temp 4 : 28°C</h5>
                             </div>
                             <div>
-                                <h5>Suhu 5 : 28°C</h5>
+                                <h5>Temp 5 : 28°C</h5>
                             </div>
                             <div>
-                                <h5>Suhu 6 : 29°C</h5>
+                                <h5>Temp 6 : 29°C</h5>
                             </div>
                             <div>
-                                <h5>Suhu 7 : 28°C</h5>
+                                <h5>Temp 7 : 28°C</h5>
                             </div>
                         </div>
                     </div>
@@ -162,32 +140,33 @@
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2 text-center"> <!-- Tambahkan class text-center -->
                         <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                            Sensor Kelembaban
+                            Humidity Sensor
                         </div>
-                        <div>
-                            <h5>Kelembapan 1 : 50%</h5>
                             <div>
-                                <h5>Kelembapan 2 : 50%</h5>
+                                <h5>Humidity 1 : 50%</h5>
+                            </div>    
+                            <div>
+                                <h5>Humidity 2 : 50%</h5>
                             </div>
                             <div>
-                                <h5>Kelembapan 3 : 50%</h5>
+                                <h5>Humidity 3 : 50%</h5>
                             </div>
                             <div>
-                                <h5>Kelembapan 4 : 50%</h5>
+                                <h5>Humidity 4 : 50%</h5>
                             </div>
                             <div>
-                                <h5>Kelembapan 5 : 50%</h5>
+                                <h5>Humidity 5 : 50%</h5>
                             </div>
                             <div>
-                                <h5>Kelembapan 6 : 50%</h5>
+                                <h5>Humidity 6 : 50%</h5>
                             </div>
                             <div>
-                                <h5>Kelembapan 7 : 50%</h5>
+                                <h5>Humidity 7 : 50%</h5>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-auto">
-                    <i class="fas fa-tint"></i>
+                        <div class="col-auto">
+                            <i class="fas fa-tint"></i>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -205,5 +184,452 @@
 @endsection
 
 @section('script')
-
+<script nonce="undefined" src="https://cdn.zingchart.com/zingchart.min.js"></script>
+<script>
+    ZC.LICENSE = ["569d52cefae586f634c54f86dc99e6a9", "b55b025e438fa8a98e32482b5f768ff5"];
+    window.feed = function(callback) {
+      var tick = {};
+      tick.plot0 = Math.ceil(0 + (Math.random() * 100));
+      callback(JSON.stringify(tick));
+    };
+    var myConfig = {
+      type: "gauge",
+      globals: {
+        fontSize: 20
+      },
+      plotarea: {
+        marginTop: 130
+      },
+      plot: {
+        size: '100%',
+        valueBox: {
+          placement: 'center',
+          text: '%v', //default
+          fontSize: 24,
+          
+          
+        }
+      },
+      tooltip: {
+        borderRadius: 5,
+      },
+      scaleR: {
+        aperture: 180,
+        minValue: 0,
+        maxValue: 100,
+        step: 50,
+        center: {
+          visible: false
+        },
+        tick: {
+          visible: false
+        },
+        item: {
+          offsetR: 0,
+          rules: [{
+            rule: '%i == 9',
+            offsetX: 15
+          }]
+        },
+        labels: ['0', '50', '100'],
+        ring: {
+          size: 50,
+          rules: [{
+          rule: '%v <= 100',
+          backgroundColor: '#FF6500'
+        }]
+        }
+      },
+      refresh: {
+        type: "feed",
+        transport: "js",
+        url: "feed()",
+        interval: 1500,
+        resetTimeout: 1000
+      },
+      series: [{
+        values: [50], // starting value
+        backgroundColor: 'black',
+        indicator: [10, 10, 10, 10, 0.65],
+        animation: {
+          effect: 2,
+          method: 1,
+          sequence: 4,
+          speed: 1000
+        },
+      }]
+    };
+    var kelembabanConfig = {
+      type: "gauge",
+      globals: {
+        fontSize: 20
+      },
+      plotarea: {
+        marginTop: 130
+      },
+      plot: {
+        size: '100%',
+        valueBox: {
+          placement: 'center',
+          text: '%v', //default
+          fontSize: 24,
+          
+          
+        }
+      },
+      tooltip: {
+        borderRadius: 5,
+      },
+      scaleR: {
+        aperture: 180,
+        minValue: 0,
+        maxValue: 100,
+        step: 50,
+        center: {
+          visible: false
+        },
+        tick: {
+          visible: false
+        },
+        item: {
+          offsetR: 0,
+          rules: [{
+            rule: '%i == 9',
+            offsetX: 15
+          }]
+        },
+        labels: ['0', '50', '100'],
+        ring: {
+          size: 50,
+          rules: [{
+          rule: '%v <= 100',
+          backgroundColor: '#FF6500'
+        }]
+        }
+      },
+      refresh: {
+        type: "feed",
+        transport: "js",
+        url: "feed()",
+        interval: 1500,
+        resetTimeout: 1000
+      },
+      series: [{
+        values: [50], // starting value
+        backgroundColor: 'black',
+        indicator: [10, 10, 10, 10, 0.65],
+        animation: {
+          effect: 2,
+          method: 1,
+          sequence: 4,
+          speed: 1000
+        },
+      }]
+    };
+    var co2Config = {
+      type: "gauge",
+      globals: {
+        fontSize: 20
+      },
+      plotarea: {
+        marginTop: 130
+      },
+      plot: {
+        size: '100%',
+        valueBox: {
+          placement: 'center',
+          text: '%v', //default
+          fontSize: 24,
+          
+          
+        }
+      },
+      tooltip: {
+        borderRadius: 5,
+      },
+      scaleR: {
+        aperture: 180,
+        minValue: 0,
+        maxValue: 100,
+        step: 50,
+        center: {
+          visible: false
+        },
+        tick: {
+          visible: false
+        },
+        item: {
+          offsetR: 0,
+          rules: [{
+            rule: '%i == 9',
+            offsetX: 15
+          }]
+        },
+        labels: ['0', '50', '100'],
+        ring: {
+          size: 50,
+          rules: [{
+          rule: '%v <= 100',
+          backgroundColor: '#FF6500'
+        }]
+        }
+      },
+      refresh: {
+        type: "feed",
+        transport: "js",
+        url: "feed()",
+        interval: 1500,
+        resetTimeout: 1000
+      },
+      series: [{
+        values: [50], // starting value
+        backgroundColor: 'black',
+        indicator: [10, 10, 10, 10, 0.65],
+        animation: {
+          effect: 2,
+          method: 1,
+          sequence: 4,
+          speed: 1000
+        },
+      }]
+    };
+    var airConfig = {
+      type: "gauge",
+      globals: {
+        fontSize: 20
+      },
+      plotarea: {
+        marginTop: 130
+      },
+      plot: {
+        size: '100%',
+        valueBox: {
+          placement: 'center',
+          text: '%v', //default
+          fontSize: 24,
+          
+          
+        }
+      },
+      tooltip: {
+        borderRadius: 5,
+      },
+      scaleR: {
+        aperture: 180,
+        minValue: 0,
+        maxValue: 100,
+        step: 50,
+        center: {
+          visible: false
+        },
+        tick: {
+          visible: false
+        },
+        item: {
+          offsetR: 0,
+          rules: [{
+            rule: '%i == 9',
+            offsetX: 15
+          }]
+        },
+        labels: ['0', '50', '100'],
+        ring: {
+          size: 50,
+          rules: [{
+          rule: '%v <= 100',
+          backgroundColor: '#FF6500'
+        }]
+        }
+      },
+      refresh: {
+        type: "feed",
+        transport: "js",
+        url: "feed()",
+        interval: 1500,
+        resetTimeout: 1000
+      },
+      series: [{
+        values: [50], // starting value
+        backgroundColor: 'black',
+        indicator: [10, 10, 10, 10, 0.65],
+        animation: {
+          effect: 2,
+          method: 1,
+          sequence: 4,
+          speed: 1000
+        },
+      }]
+    };
+    var cahayaConfig = {
+      type: "gauge",
+      globals: {
+        fontSize: 20
+      },
+      plotarea: {
+        marginTop: 130
+      },
+      plot: {
+        size: '100%',
+        valueBox: {
+          placement: 'center',
+          text: '%v', //default
+          fontSize: 24,
+          
+          
+        }
+      },
+      tooltip: {
+        borderRadius: 5,
+      },
+      scaleR: {
+        aperture: 180,
+        minValue: 0,
+        maxValue: 100,
+        step: 50,
+        center: {
+          visible: false
+        },
+        tick: {
+          visible: false
+        },
+        item: {
+          offsetR: 0,
+          rules: [{
+            rule: '%i == 9',
+            offsetX: 15
+          }]
+        },
+        labels: ['0', '50', '100'],
+        ring: {
+          size: 50,
+          rules: [{
+          rule: '%v <= 100',
+          backgroundColor: '#FF6500'
+        }]
+        }
+      },
+      refresh: {
+        type: "feed",
+        transport: "js",
+        url: "feed()",
+        interval: 1500,
+        resetTimeout: 1000
+      },
+      series: [{
+        values: [50], // starting value
+        backgroundColor: 'black',
+        indicator: [10, 10, 10, 10, 0.65],
+        animation: {
+          effect: 2,
+          method: 1,
+          sequence: 4,
+          speed: 1000
+        },
+      }]
+    };
+    var panelConfig = {
+      type: "gauge",
+      globals: {
+        fontSize: 20
+      },
+      plotarea: {
+        marginTop: 130
+      },
+      plot: {
+        size: '100%',
+        valueBox: {
+          placement: 'center',
+          text: '%v', //default
+          fontSize: 24,
+          
+          
+        }
+      },
+      tooltip: {
+        borderRadius: 5,
+      },
+      scaleR: {
+        aperture: 180,
+        minValue: 0,
+        maxValue: 100,
+        step: 50,
+        center: {
+          visible: false
+        },
+        tick: {
+          visible: false
+        },
+        item: {
+          offsetR: 0,
+          rules: [{
+            rule: '%i == 9',
+            offsetX: 15
+          }]
+        },
+        labels: ['0', '50', '100'],
+        ring: {
+          size: 50,
+          rules: [{
+          rule: '%v <= 100',
+          backgroundColor: '#FF6500'
+        }]
+        }
+      },
+      refresh: {
+        type: "feed",
+        transport: "js",
+        url: "feed()",
+        interval: 1500,
+        resetTimeout: 1000
+      },
+      series: [{
+        values: [50], // starting value
+        backgroundColor: 'black',
+        indicator: [10, 10, 10, 10, 0.65],
+        animation: {
+          effect: 2,
+          method: 1,
+          sequence: 4,
+          speed: 1000
+        },
+      }]
+    };
+ 
+    zingchart.render({
+      id: 'myChart',
+      data: myConfig,
+      height: 400,
+      width: '100%'
+    });
+    zingchart.render({
+      id: 'kelembabanChart',
+      data: kelembabanConfig,
+      height: 400,
+      width: '100%'
+    });
+    zingchart.render({
+      id: 'airChart',
+      data: airConfig,
+      height: 400,
+      width: '100%'
+    });
+    zingchart.render({
+      id: 'co2Chart',
+      data: co2Config,
+      height: 400,
+      width: '100%'
+    });
+    zingchart.render({
+      id: 'cahayaChart',
+      data: cahayaConfig,
+      height: 400,
+      width: '100%'
+    });
+    zingchart.render({
+      id: 'panelChart',
+      data: panelConfig,
+      height: 400,
+      width: '100%'
+    });
+</script>
 @endsection
