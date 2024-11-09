685e3c1ca14ff6bdda5737dab927f20634d59535
 <anif2805@gmail.com>
2024-11-09 10:21:35 +0700

    tes lagi

--- resources/views/greenhouse/index.blade.php
+++ resources/views/greenhouse/index.blade.php
@@ -235,6 +235,22 @@
                 </div>
             </div>
         </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Room Panel Temp</div>
+                            <div>28°C</div>
+                        </div>
+                        <div class="col-auto">
+                        <i class="fas fa-laptop-house"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
     <div class="row">
