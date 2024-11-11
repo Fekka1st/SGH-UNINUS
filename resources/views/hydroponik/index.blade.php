57f007cc8ed47a26f202a64bbf74da0bd51540fe
Bokoboy12 <boykeadam08@gmail.com>
2024-11-11 13:51:13 +0700

    fix

--- resources/views/hydroponik/index.blade.php
+++ resources/views/hydroponik/index.blade.php
@@ -76,7 +76,7 @@
                 </div>
             </div>
 
-            <div class="col-xl-6 col-md-6 mb-4">
+            <div class="col-xl-4 col-md-6 mb-4">
                 <div class="card border-left-primary shadow h-100 py-2">
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
@@ -98,7 +98,7 @@
                 </div>
             </div>
 
-            <div class="col-xl-6 col-md-6 mb-4">
+            <div class="col-xl-4 col-md-6 mb-4">
                 <div class="card border-left-primary shadow h-100 py-2">
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
